8973213d2801151f317fb1dc3761a69bf5b77b8e
unknown <196222578+akk0020@users.noreply.github.com>
2025-03-25 20:48:01 -0500

    api calls for home.js and pizz.js

---
+++ store/order.js
@@ -0,0 +1,4 @@
+export default {
+  header: "order page man", // The text that shows on the view
+  view: "order", // This should match the view file name
+};
---
+++ store/viewNotFound.js
@@ -0,0 +1,4 @@
+export default {
+  header: "wrong page man", // The text that shows on the view
+  view: "viewNotFound", // This should match the view file name
+};
